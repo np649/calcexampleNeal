e5a7205399ba06f77df525740586bc03439df437
np649 <65683440+np649@users.noreply.github.com>
2021-12-15 15:40:35 -0500

    updated csv's

--- calc/addition.py
+++ calc/addition.py
@@ -1,4 +1,4 @@
-"""this is the addition calculation that is being inherits the value
+"""this is the adition calculation that is being inherits the value
 A and value B from the calculation class"""
 
 from calc.calculation import Calculation
