d6f7fa72539cd9b648283b271e0083afee145a18
np649 <65683440+np649@users.noreply.github.com>
2021-12-15 15:42:01 -0500

    Create calculator_test.py

---
+++ tests/calculator_test.py
@@ -0,0 +1,55 @@
+"""Testing the Calculator"""
+import pprint
+
+import pytest
+
+from calculator.calculator import Calculator
+@pytest.fixture
+def clear_history():
+    Calculator.clear_history()
+
+def test_calculator_add(clear_history):
+    """testing the add function of the calculator"""
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 6
+    pprint.pprint(Calculator.history)
+
+def test_clear_history(clear_history):
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.clear_history() == True
+    assert Calculator.history_count() == 0
+
+def test_count_history(clear_history):
+    assert Calculator.history_count() == 0
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.history_count() == 2
+
+def test_get_last_calculator_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 5
+
+def test_get_first_calculator_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_first_calculation_added_to_history() == 4
+
+def test_calculator_subtract(clear_history):
+    """Testing the subtract method of the calculator"""
+    assert Calculator.subtract_number(1,2) == -1
+
+def test_calculator_multiply(clear_history):
+    """test multiplication of two numbers"""
+    assert Calculator.multiply_numbers(1,2) == 2
+def test_calculator_divide(clear_history):
+    """test division of two numbers"""
+    assert Calculator.divide_numbers(4,2) == 2
